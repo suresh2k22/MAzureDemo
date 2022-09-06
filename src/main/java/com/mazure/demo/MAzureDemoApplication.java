206974cbbe8c956d257da49804257323bc0adfb6
SURESH MAHALINGAM <STS135@surgetechinc.in>
2022-09-06 10:39:06 +0530

    first commit

---
+++ src/main/java/com/mazure/demo/MAzureDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mazure.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MAzureDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MAzureDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mazure/demo/MAzureDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mazure.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MAzureDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
